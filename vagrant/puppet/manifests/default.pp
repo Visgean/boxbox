ee323b96468dc672c148467a333714f762db9e66
Visgean Skeloru <visgean@gmail.com>
2014-04-24 01:19:29 +0200

    umm

--- vagrant/puppet/manifests/default.pp
+++ vagrant/puppet/manifests/default.pp
@@ -1,25 +1,7 @@
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
-exec { 'apt-get update':
-  command => 'apt-get update',
-  timeout => 60,
-  tries   => 3
-}
 
-class { 'apt':
-  always_apt_update => true,
-}
-
-package { ['python-software-properties']:
-  ensure  => 'installed',
-  require => Exec['apt-get update'],
-}
-
-$sysPackages = [ 'build-essential', 'git', 'curl']
-package { $sysPackages:
-  ensure => "installed",
-  require => Exec['apt-get update'],
-}
+class { 'puppi': }
 
 class { "mysql":
   root_password => '12345',
